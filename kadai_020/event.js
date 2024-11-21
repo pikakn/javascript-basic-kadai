e2e30f85259f34a479e7711949b9e14a60fca47e
pikakn <akanaritamade@outlook.jp>
2024-11-21 18:41:51 +0900

    20章課題

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click",()=>{
+    let textChange = document.getElementById("text");
+    textChange.textContent = "ボタンをクリックしました"
+})
